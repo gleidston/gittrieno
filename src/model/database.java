ce29cd3054999f09a5251233ee419fb6e90cd49e
Gleidston <king2010-ro@hotmail.com>
2020-04-11 09:14:58 -0300

    c5 - preparando para comando rebase on master branch

---
+++ src/model/database.java
@@ -0,0 +1,59 @@
+package model;
+
+public class database {
+
+  String lol ;
+  String nome ;
+  int numero ;
+  int cpf ;
+  double total ;
+  float  clal ;
+
+  public String getLol() {
+    return lol;
+  }
+
+  public void setLol(String lol) {
+    this.lol = lol;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(int cpf) {
+    this.cpf = cpf;
+  }
+
+  public double getTotal() {
+    return total;
+  }
+
+  public void setTotal(double total) {
+    this.total = total;
+  }
+
+  public float getClal() {
+    return clal;
+  }
+
+  public void setClal(float clal) {
+    this.clal = clal;
+  }
+}
